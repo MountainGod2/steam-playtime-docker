25a8fe3cd9c708b2475c54ebfdbf2a74bdb004b2
MountainGod2 <admin@reid.ca>
2026-07-20 16:42:26 -0600

    refactor(verify_upstream): update upstream verification with parameters from workflow

--- .github/workflows/verify_upstream.sh
+++ .github/workflows/verify_upstream.sh
@@ -2,18 +2,22 @@
 
 set -euo pipefail
 
-if ! upstream_branch="$(
-    git rev-parse --abbrev-ref --symbolic-full-name '@{upstream}'
-)"; then
+if [[ "$#" -ne 2 || -z "$1" || -z "$2" ]]; then
     printf '%s\n' "::error::Unable to determine upstream branch name!" >&2
     exit 1
 fi
 
-remote_name="${upstream_branch%%/*}"
+remote_name="$1"
+branch_name="$2"
+upstream_branch="$remote_name/$branch_name"
 
 git fetch "$remote_name"
 
-upstream_sha="$(git rev-parse "$upstream_branch")"
+if ! upstream_sha="$(git rev-parse "$upstream_branch")"; then
+    printf '%s\n' "::error::Unable to determine upstream branch name!" >&2
+    exit 1
+fi
+
 head_sha="$(git rev-parse HEAD)"
 
 if [[ "$head_sha" != "$upstream_sha" ]]; then
